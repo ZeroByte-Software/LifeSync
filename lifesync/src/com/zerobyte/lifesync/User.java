8fb8c9ac33fc03a65123f25d80411b372daea525
Andrew <sky417@gmail.com>
2012-10-28 22:50:09 -0700

    Added Admin.java, User.java class file (skeleton only) Added testAdmin, testUser JUnit

---
+++ lifesync/src/com/zerobyte/lifesync/User.java
@@ -0,0 +1,44 @@
+package com.zerobyte.lifesync;
+
+public class User {
+	
+	/**
+	 * skeleton class to create the junit test
+	 */
+	
+	private String firstName;
+	private String lastName;
+	private int userID;
+	
+	public User(int id) {
+		
+	}
+	
+	public User(int id, String firstName, String lastName) {
+		
+	}
+	
+	public String getFirstName() {
+		
+		return null;
+	}
+	
+	public String getLastName() {
+		
+		return null;
+	}
+	
+	public int getID() {
+		
+		return 0;
+	}
+	
+	public void setFirstName(String firstName) {
+		
+	}
+	
+	public void setLastName(String lastName) {
+		
+	}
+	
+}
---
+++ lifesync/src/com/zerobyte/lifesync/test/testUser.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.zerobyte.lifesync.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author andrewyoon
+ *
+ */
+public class testUser {
+
+	/**
+	 * Test method for {@link com.zerobyte.lifesync.User#User(int)}.
+	 */
+	@Test
+	public void testUserInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.zerobyte.lifesync.User#User(int, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testUserIntStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.zerobyte.lifesync.User#getFirstName()}.
+	 */
+	@Test
+	public void testGetFirstName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.zerobyte.lifesync.User#getLastName()}.
+	 */
+	@Test
+	public void testGetLastName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.zerobyte.lifesync.User#getID()}.
+	 */
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.zerobyte.lifesync.User#setFirstName(java.lang.String)}.
+	 */
+	@Test
+	public void testSetFirstName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.zerobyte.lifesync.User#setLastName(java.lang.String)}.
+	 */
+	@Test
+	public void testSetLastName() {
+		fail("Not yet implemented");
+	}
+
+}
