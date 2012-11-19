1a35a3da20c9a693cd2b621f5a0bc0ab33fa2976
Andrew <sky417@gmail.com>
2012-11-18 19:22:09 -0800

    commented out bump stuff

--- lifesync/src/com/zerobyte/lifesync/AndroidTabLayoutActivity.java
+++ lifesync/src/com/zerobyte/lifesync/AndroidTabLayoutActivity.java
@@ -7,8 +7,17 @@ import java.util.List;
 import java.util.Map;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -37,6 +46,68 @@ public class AndroidTabLayoutActivity extends Activity {
 	List<ArrayList<TimeSlot>> time_slots_data = null;
 
 	// FRIEND LIST VARIABLES
+
+	// BUMP VARIABLES
+
+	/*private IBumpAPI api;
+
+	private final ServiceConnection connection = new ServiceConnection() {
+		@Override
+		public void onServiceConnected(ComponentName className, IBinder binder) {
+			Log.i("BumpTest", "onServiceConnected");
+			api = IBumpAPI.Stub.asInterface(binder);
+			try {
+				api.configure("2156db3846b54a2693f1ddfab9db3b8f", "Bump User");
+			} catch (RemoteException e) {
+				Log.w("BumpTest", e);
+			}
+			Log.d("Bump Test", "Service connected");
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName className) {
+			Log.d("Bump Test", "Service disconnected");
+		}
+	};
+
+	private final BroadcastReceiver receiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			final String action = intent.getAction();
+			try {
+				if (action.equals(BumpAPIIntents.DATA_RECEIVED)) {
+					Log.i("Bump Test",
+							"Received data from: "
+									+ api.userIDForChannelID(intent
+											.getLongExtra("channelID", 0)));
+					Log.i("Bump Test",
+							"Data: "
+									+ new String(intent
+											.getByteArrayExtra("data")));
+				} else if (action.equals(BumpAPIIntents.MATCHED)) {
+					long channelID = intent
+							.getLongExtra("proposedChannelID", 0);
+					Log.i("Bump Test",
+							"Matched with: "
+									+ api.userIDForChannelID(channelID));
+					api.confirm(channelID, true);
+					Log.i("Bump Test", "Confirm sent");
+				} else if (action.equals(BumpAPIIntents.CHANNEL_CONFIRMED)) {
+					long channelID = intent.getLongExtra("channelID", 0);
+					Log.i("Bump Test",
+							"Channel confirmed with "
+									+ api.userIDForChannelID(channelID));
+					api.send(channelID, "Hello, world!".getBytes());
+				} else if (action.equals(BumpAPIIntents.NOT_MATCHED)) {
+					Log.i("Bump Test", "Not matched.");
+				} else if (action.equals(BumpAPIIntents.CONNECTED)) {
+					Log.i("Bump Test", "Connected to Bump...");
+					api.enableBumping();
+				}
+			} catch (RemoteException e) {
+			}
+		}
+	};*/
 
 	/** Called when the activity is first created. */
 	@Override
@@ -87,15 +158,17 @@ public class AndroidTabLayoutActivity extends Activity {
 				}
 				time_slots_data.add(time_slots_by_time);
 			}
-			
-			ScheduleEvent se = new ScheduleEvent("lol", "1-4", "2-6", "here", "test man", "Self");
+
+			ScheduleEvent se = new ScheduleEvent("lol", "1-4", "2-6", "here",
+					"test man", "Self");
 			schedule_data.put(se.getEvent_id(), se);
-			
+
 			init_flag = true;
 		}
 
 		update_time_slots_data();
-		schedule_adapter = new ScheduleListAdapter(this, time_slots_data, schedule_data);
+		schedule_adapter = new ScheduleListAdapter(this, time_slots_data,
+				schedule_data);
 
 		schedule_listView = (ListView) findViewById(R.id.schedule_list);
 		// listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
@@ -112,23 +185,70 @@ public class AndroidTabLayoutActivity extends Activity {
 
 		// FRIEND LIST LOGIC
 
-		
-		
-		// TODO BUMP LOGIC GOES HERE
+		// TODO for andrew
+		// BUMP LOGIC GOES HERE
+
+		/*super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bump);
+
+		bindService(new Intent(IBumpAPI.class.getName()),
+				connection, Context.BIND_AUTO_CREATE);
+		Log.i("BumpTest", "boot");
+
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(BumpAPIIntents.CHANNEL_CONFIRMED);
+		filter.addAction(BumpAPIIntents.DATA_RECEIVED);
+		filter.addAction(BumpAPIIntents.NOT_MATCHED);
+		filter.addAction(BumpAPIIntents.MATCHED);
+		filter.addAction(BumpAPIIntents.CONNECTED);
+		registerReceiver(receiver, filter);
 		
 		Button bumpbtn = (Button) findViewById(R.id.btnBump);
-//		bumpbtn.setOnClickListener(new View.OnClickListener() {
-//			public void onClick(View v) {
-//				String email = editTxtEmail.getText().toString();
-//				String password = editTxtPassword.getText().toString();
-//				
-//				if( email.equals("") || password.equals("") )
-//					showToast( "Please enter your email and password." );
-//				else
-//					login(email, password);
-//			}
-//		});
-		
+		bumpbtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				class BumpTest extends Activity {
+
+					@Override
+					public void onConfigurationChanged(Configuration newConfig) {
+						super.onConfigurationChanged(newConfig);
+					}
+
+
+//					public void onStart() {
+//						Log.i("BumpTest", "onStart");
+//						super.onStart();
+//					}
+//
+//					public void onRestart() {
+//						Log.i("BumpTest", "onRestart");
+//						super.onRestart();
+//					}
+//
+//					public void onResume() {
+//						Log.i("BumpTest", "onResume");
+//						super.onResume();
+//					}
+//
+//					public void onPause() {
+//						Log.i("BumpTest", "onPause");
+//						super.onPause();
+//					}
+//
+//					public void onStop() {
+//						Log.i("BumpTest", "onStop");
+//						super.onStop();
+//					}
+
+					public void onDestroy() {
+						Log.i("BumpTest", "onDestroy");
+						unbindService(connection);
+						unregisterReceiver(receiver);
+						super.onDestroy();
+					}
+				}
+			}
+		});*/
+
 	}
 
 	@Override
@@ -184,9 +304,8 @@ public class AndroidTabLayoutActivity extends Activity {
 						event_data.get("event_start_time"),
 						event_data.get("event_end_time"),
 						event_data.get("event_location"),
-						event_data.get("event_description"),
-						"Self");
-				
+						event_data.get("event_description"), "Self");
+
 				schedule_data.put(se.getEvent_id(), se);
 				update_time_slots_data();
 
@@ -199,7 +318,7 @@ public class AndroidTabLayoutActivity extends Activity {
 
 	public void update_time_slots_data() {
 		if (schedule_data.size() > 0) {
-			
+
 			for (ScheduleEvent se : schedule_data.values()) {
 				// SUPPOSE FORMAT WAS: DAY-TIME
 				String event_start_time_str[] = se.getEvent_start_time().split(
@@ -228,9 +347,9 @@ public class AndroidTabLayoutActivity extends Activity {
 						time_slots_data.get(j).get(i).setStatus(1);
 						time_slots_data.get(j).get(i).addEvent(se);
 					}
-					
+
 					// SPECIAL CASE FOR 23:00-00:00
-					if(event_end_time[0] == (i + 1)) {
+					if (event_end_time[0] == (i + 1)) {
 						time_slots_data.get(23).get(i).setStatus(1);
 						time_slots_data.get(23).get(i).addEvent(se);
 					}
